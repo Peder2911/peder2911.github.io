c295046a22a01786f7e074d03844bb621e02bd1d
peder2911 <pglandsverk@gmail.com>
2020-09-11 16:40:50 +0200

    Trying vanilla AJAX

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 
-console.log("hello world")
+
+let xhr = new XMLHttpRequest();
+let data
+
+let addProjects = function(data){
+   let projects = document.querySelector("div#projects");
+   Object.entries(data).forEach((pair)=>{
+      let el = document.createElement("div")
+      el.innerText = `${pair[0]} : ${pair[1]}`
+      projects.appendChild(el)
+   })
+}
+
+xhr.onload = function(){
+   if(xhr.status >= 200 && xhr.status < 300){
+      try {
+         data = JSON.parse(xhr.response)
+      } catch(e) {
+         console.log(e)
+      }
+      addProjects(data)
+   } else {
+      console.log(xhr.status)
+   }
+}
+
+xhr.open("GET","data/projects.json");
+xhr.send();
